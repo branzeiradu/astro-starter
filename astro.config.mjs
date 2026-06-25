6504c26cbf50ec51910edb75945fd49cba2e7fce
Radu Branzei <radub@ensemble.com>
2026-06-25 12:06:26 +0300

    Update dependecies to make astro work with cloudflare and tailwind.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 
-import tailwind from "@astrojs/tailwind";
+import cloudflare from '@astrojs/cloudflare';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
-  integrations: [tailwind()]
+  adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
